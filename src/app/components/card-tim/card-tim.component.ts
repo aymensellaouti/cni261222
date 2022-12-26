cb3d8b050f3941e9576d3ca6517332a2150c3fc0
aymen2782 <aymen.sellaouti@gmail.com>
2022-12-26 15:07:39 +0100

    exo card two way data binding

--- src/app/components/card-tim/card-tim.component.ts
+++ src/app/components/card-tim/card-tim.component.ts
@@ -3,8 +3,11 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-card-tim',
   templateUrl: './card-tim.component.html',
-  styleUrls: ['./card-tim.component.css']
+  styleUrls: ['./card-tim.component.css'],
 })
 export class CardTimComponent {
-
+  name = 'Sellaouti';
+  firstname = 'Aymen';
+  job = 'Teacher';
+  path = 'rotating_card_profile3.png';
 }
